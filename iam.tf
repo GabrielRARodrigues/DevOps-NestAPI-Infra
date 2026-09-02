aa71fca2afe5634b17242da6a0a28cfd00eb3dbb
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2026-09-02 16:44:23 -0300

    new: configure backend

--- iam.tf
+++ iam.tf
@@ -56,6 +56,12 @@ resource "aws_iam_role_policy" "terraform_permission_policy" {
         Action   = "iam:*"
         Effect   = "Allow"
         Resource = "*"
+      },
+      {
+        Sid      = "Statement3"
+        Action   = "s3:*"
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
--- main.tf
+++ main.tf
@@ -5,8 +5,37 @@ terraform {
       version = "6.60.0"
     }
   }
+
+  backend "s3" {
+    bucket = "devops-nest-api-iac-state"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
 provider "aws" {
   region = "us-east-2"
+}
+
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "devops-nest-api-iac-state"
+
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_bucket" {
+  bucket = aws_s3_bucket.terraform_state_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
